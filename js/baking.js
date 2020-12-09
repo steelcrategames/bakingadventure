469bfc572504b305d0d8f1aee39d2f3c229ed1a9
Brian Fetter <brian.fetter.dev@gmail.com>
2020-12-09 14:40:13 -0500

    Added multiselect to baking page.

--- js/baking.js
+++ js/baking.js
@@ -3,31 +3,38 @@ class Inventory
     constructor()
     {
         this.Ingredients = new Map();
+        this.IngredientAmounts = new Map();
     }
 
     hasIngredient(ingredient)
     {
-        return this.Ingredients.has(ingredient);
+        return this.IngredientAmounts.has(ingredient);
     }
 
     addIngredient(ingredient, quantity)
     {
         if(!this.hasIngredient(ingredient))
         {
-            this.Ingredients.set(ingredient, quantity);
+            this.IngredientAmounts.set(ingredient, quantity);
+            this.Ingredients.set(ingredient.name, ingredient);
         }
         else
         {
-            var q = this.Ingredients.get(ingredient) + quantity;
+            var q = this.IngredientAmounts.get(ingredient) + quantity;
             if(q < 0)
                 q = 0;
-            this.Ingredients.set(ingredient, q);
+            this.IngredientAmounts.set(ingredient, q);
         }
     }
 
     removeIngredient(ingredient, quantity)
     {
         this.addIngredient(ingredient, -quantity);
+    }
+
+    getIngredient(name)
+    {
+        return this.Ingredients.get(name);
     }
 }
 
@@ -47,16 +54,40 @@ class BakedGood
     }
 }
 
+var inventory;
+var selected = new Array();
+
 function loadInventory(container)
 {
-    var inventory = new Inventory();
+    inventory = new Inventory();
     inventory.addIngredient(new Ingredient("Eggs"), 1);
     inventory.addIngredient(new Ingredient("Flour"), 1);
     inventory.addIngredient(new Ingredient("Salt"), 1);
     inventory.addIngredient(new Ingredient("Sugar"), 1);
 
-    inventory.Ingredients.forEach(element => {
-        
+    inventory.IngredientAmounts.forEach((value, key) => {
+        var id = key.name;
+        var item = document.createElement("div");
+        item.setAttribute("id", id);
+        item.setAttribute("class", "inventory-item");
+        item.innerText = key.name;
+        $(item).click(function() {
+            if(selected.indexOf(item.id) > -1)
+            {
+                selected.splice(selected.indexOf(item.id), 1);
+                item.style.backgroundColor = "#FFFFFF";
+            }
+            else
+            {
+                selected.push(item.id);
+                item.style.backgroundColor = "#2F4F4F";
+            }
+          });
+        container.append(item);
     });
-    container.append("<div>" + "</div>");
+    
 }
+
+$( document ).ready(function() {
+    loadInventory($("#ingredients"));
+});
