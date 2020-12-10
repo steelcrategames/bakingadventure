a2980a913e59b2c4b257339c428274ebf115fc34
bkane <ben@keeptalkinggame.com>
2020-12-10 12:02:46 -0500

    Flesh out adventure flow a bit with a rough state machine

--- js/adventure.js
+++ js/adventure.js
@@ -6,33 +6,131 @@ const LOG_TYPES = {
     BAD: "bg-danger"
 }
 
+const ADVENTURE_STATES = {
+    empty_restaurant: 
+    {
+        onEnter: () => {
+                clearLog();
+                log("The restaurant is empty");
+                showButton("meetNewAdventurerButton");
+
+                hideButton("doQuestIntroButton");
+                hideButton("giveFoodToAdventurerButton");
+                hideButton("doQuestRecapButton");
+             },
+             onExit: () => 
+             {
+                hideButton("meetNewAdventurerButton");
+             }
+    },
+
+    intro: 
+    {
+        onEnter: doAdventurerIntro
+    },
+
+    describe_quest: 
+    {
+        onEnter: () => showButton("doQuestIntroButton"),
+        onExit: () => hideButton("doQuestIntroButton")
+    },
+
+    cooking:
+    {
+        onEnter: () => showButton("giveFoodToAdventurerButton"),
+        onExit: () => hideButton("giveFoodToAdventurerButton")
+    },
+
+    quest_recap: 
+    {
+        onEnter: doQuestRecap
+    }
+}
+
+class StateMachine {
+    constructor(definition)
+    {
+        this.states = definition;
+        this.current_state = null;
+    }
+
+    changeState(state)
+    {
+        if (this.current_state != null)
+        {
+            if (this.current_state.onExit != null)
+            {
+                this.current_state.onExit();
+            }
+        }
+
+        this.current_state = state;
+
+        if (this.current_state.onEnter != null)
+        {
+            this.current_state.onEnter();
+        }
+    }
+}
+
+class Adventurer
+{
+    constructor(name, title, quote, appetite)
+    {
+        this.name = name;
+        this.title = title;
+        this.quote = quote;
+
+        this.atk = getRndInteger(1, 5),
+        this.def =  getRndInteger(1, 5),
+        this.hp =  getRndInteger(3, 8),
+        this.appetite = appetite;
+
+        this.food = null;
+    }
+
+    setFood(food)
+    {
+        this.food = food;
+    }
+}
+
 var currentAdventurer = {};
+    var adventureStateMachine = new StateMachine(ADVENTURE_STATES);
+
+window.onload = () => {
+    adventureStateMachine.changeState(ADVENTURE_STATES.empty_restaurant);
+}
 
 function createRandomAdventurer()
 {
-    var appetites = [ "small", "medium", "large" ];
-    var skillLevels = [ "Novice", "Apprentice", "Experienced", "Savvy" ];
-    var titles = [ "Knight", "Squire", "Bard", "Adventurer", "Farmer" ];
+    const appetites = [ "small", "medium", "large" ];
+    const skillLevels = [ "Novice", "Apprentice", "Experienced", "Savvy" ];
+    const titles = [ "Knight", "Squire", "Bard", "Adventurer", "Farmer" ];
 
-    var title = skillLevels[Math.floor(Math.random() * skillLevels.length)] + " " + titles[Math.floor(Math.random() * titles.length)];
+    let name = String.fromCharCode(65+Math.floor(Math.random() * 26)) + "immy";
+    let title = "the" + skillLevels[Math.floor(Math.random() * skillLevels.length)] + " " + titles[Math.floor(Math.random() * titles.length)];
+    let quote = name + "'s the name and adventuring is my game!";
 
-    var name = String.fromCharCode(65+Math.floor(Math.random() * 26)) + "immy";
+    let appetite = appetites[Math.floor(Math.random() * appetites.length)];
 
-    return {
-        name: name,
-        title: "the " + title,
-        quote: name + "'s the name and adventuring is my game!",
-        atk: getRndInteger(1, 5),
-        def:  getRndInteger(1, 5),
-        hp:  getRndInteger(3, 8),
-        appetite: appetites[Math.floor(Math.random() * appetites.length)]
-    };
+    return new Adventurer(name, title, quote, appetite);
 }
 
 //Random integer, in range [min, max]
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
+
+function showButton(buttonName)
+{
+    document.getElementById(buttonName).style.display = "block";
+}
+
+function hideButton(buttonName)
+{
+    document.getElementById(buttonName).style.display = "none";
+}
 
 function updateAdventurerStatBox()
 {
@@ -43,10 +141,25 @@ function updateAdventurerStatBox()
     document.getElementById("adventurerDEF").innerText = currentAdventurer.def;
     document.getElementById("adventurerHP").innerText = currentAdventurer.hp;
     document.getElementById("adventurerAppetite").innerText = currentAdventurer.appetite;
+
+    if (currentAdventurer.food != null)
+    {
+        document.getElementById("adventurerFood").innerText = currentAdventurer.food.name;
+    }
+    else
+    {
+        document.getElementById("adventurerFood").innerText = "none";
+    }
+}
+
+
+function meetNewAdventurer()
+{
+    adventureStateMachine.changeState(ADVENTURE_STATES.intro);
 }
 
 //First meeting of an adventurer - do an introduction
-function doAdventurerIntro()
+function doAdventurerIntro() 
 {
     currentAdventurer = createRandomAdventurer();
     updateAdventurerStatBox();
@@ -56,6 +169,8 @@ function doAdventurerIntro()
     log("Hi, I'm " + currentAdventurer.name);
     log("I just moved into the area and heard about a great restaurant. People say it's the place to go for new adventurers!");
     log("I just bought my first armor and am really excited to go on a quest.");
+
+    adventureStateMachine.changeState(ADVENTURE_STATES.describe_quest);
 }
 
 //Before each quest - describe what lies ahead
@@ -64,6 +179,22 @@ function doQuestIntro()
     clearLog();
     log("I'm heading out to my first quest in the goblin cave.");
     log("I'm a bit nervous. Do you have anything that might help soothe my nerves?");
+
+    adventureStateMachine.changeState(ADVENTURE_STATES.cooking);
+}
+
+function giveFoodToAdventurer()
+{
+    let testFood = {
+        name: "Beef Stew",
+        atk_bonus: 1
+    };
+
+    currentAdventurer.setFood(testFood);
+    log("You gave " + testFood.name + " to " + currentAdventurer.name);
+    updateAdventurerStatBox();
+
+    adventureStateMachine.changeState(ADVENTURE_STATES.quest_recap);
 }
 
 //After each quest - recap what happened
