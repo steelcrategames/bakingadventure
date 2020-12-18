89dea9dd67926f21e97d8ecd20513cda7a8cc7dd
bkane <ben@keeptalkinggame.com>
2020-12-18 11:30:38 -0500

    Add combat stats and dialog recapping major events in combat.

--- js/simulation.js
+++ js/simulation.js
@@ -1,9 +1,63 @@
 var quest = null;
 
+class CombatantStats
+{
+    constructor()
+    {
+        this.misses = 0;
+        this.hits = 0;
+        this.atk_dealt = { }; //Raw attack damage
+        this.dmg_taken = { }; 
+        this.dmg_blocked = { };
+    }
+
+    getAtkDealtTotal()
+    {
+        let total = 0;
+        $.each(this.atk_dealt, function(name, amount)
+        {
+            total += amount;
+        });
+        return total;
+    }
+
+    getDmgTakenTotal()
+    {
+        let total = 0;
+        $.each(this.dmg_taken, function(name, amount)
+        {
+            total += amount;
+        });
+        return total;
+    }
+
+    getDmgBlockedTotal()
+    {
+        let total = 0;
+        $.each(this.dmg_blocked, function(name, amount)
+        {
+            total += amount;
+        });
+        return total;
+    }
+}
+
+class SimStats
+{
+    constructor()
+    {
+        this.num_rounds = 0;
+        this.hero = new CombatantStats();
+        this.enemy = new CombatantStats();
+    }
+}
+
 function simulateQuest(hero)
 {
     quest = hero.current_quest;
-    quest.log = [];
+    quest.log = []; //combat log
+    quest.stats = new SimStats();
+    let stats = quest.stats;
 
     //Update hero's HP, taking food into consideration
     hero.stats.hp = hero.stats.getMaxHP();
@@ -18,8 +72,10 @@ function simulateQuest(hero)
 
     while(max_rounds > 0)
     {
+        stats.num_rounds += 1;
+
         //Hero attacks first
-        let heroWin = simulateTurn(hero, enemy);
+        let heroWin = simulateTurn(hero, enemy, stats.hero, stats.enemy);
 
         if (heroWin)
         {
@@ -42,7 +98,7 @@ function simulateQuest(hero)
         }
         else
         {
-            let enemyWin = simulateTurn(enemy, hero);
+            let enemyWin = simulateTurn(enemy, hero, stats.enemy, stats.hero);
 
             if (enemyWin)
             {
@@ -60,7 +116,7 @@ function simulateQuest(hero)
     return false;
 }
 
-function simulateTurn(attacker, defender)
+function simulateTurn(attacker, defender, attacker_combat_stats, defender_combat_stats)
 {
     let isHit = rollHit(attacker.stats.getHitChance());
 
@@ -68,10 +124,14 @@ function simulateTurn(attacker, defender)
 
     if (isHit)
     {
+        attacker_combat_stats.hits += 1;
+
         let atk = attacker.stats.getAtk();
         let def = defender.stats.getDef();
         let dmg = 0;
         
+        recordAtkStats(attacker_combat_stats, atk);
+
         //Calculate the amount of damage per type, combined into one "damage" total
         $.each(atk, function(name, amount) {
             let blockedAmount = 0;
@@ -82,6 +142,9 @@ function simulateTurn(attacker, defender)
             }
 
             dmg += Math.max(0, amount - blockedAmount);
+
+            recordBlockedStats(defender_combat_stats, name, amount - dmg);
+            recordDmgTaken(defender_combat_stats, name, dmg);
         });
 
         //Format nicely
@@ -111,6 +174,28 @@ function simulateTurn(attacker, defender)
             sim_log(`${defender.name} has ${defender.stats.hp} HP remaining.`);
         }
     }
+    else
+    {
+        attacker_combat_stats.misses += 1;
+    }
+}
+
+function recordAtkStats(stats, atk)
+{
+    $.each(atk, function(name, amount)
+    {
+        stats.atk_dealt[name] = (stats.atk_dealt[name] || 0) + amount;
+    });
+}
+
+function recordBlockedStats(stats, blocked_type, blocked_amount)
+{
+    stats.dmg_blocked[blocked_type] = (stats.dmg_blocked[blocked_type] || 0) + blocked_amount;
+}
+
+function recordDmgTaken(stats, dmg_type, dmg_amount)
+{
+    stats.dmg_taken[dmg_type] = (stats.dmg_taken[dmg_type] || 0) + dmg_amount;
 }
 
 //Utility
